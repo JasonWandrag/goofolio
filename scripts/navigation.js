bd4772eb7231a916ef24bbf119633efc3b40bb1e
JasonWandrag <jasonwandrag@hotmail.com>
2022-06-02 01:07:14 +0200

    Finalize navbar animation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,8 +1,19 @@
-import { toggleActive } from "./helpers.js";
+// import { toggleActive } from "./helpers.js";
 
 const navToggleButton = document.querySelector("#nav-toggle");
 const navButtonsContainer = document.querySelector("#navbar");
 
+const toggleActive = (el) => {
+  el.classList.toggle("active");
+};
+
 navToggleButton.addEventListener("click", () =>
   toggleActive(navButtonsContainer)
 );
+
+function scrollToSection(el) {
+  console.log(el);
+  const element = document.querySelector(el);
+  element.scrollIntoView();
+  toggleActive(navButtonsContainer);
+}
